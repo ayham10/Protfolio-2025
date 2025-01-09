0ea471072c0209048982e99bbd5dc4c3ece78c0d
ayham10 <58478903+ayham10@users.noreply.github.com>
2025-01-10 00:17:54 +0200

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -52,7 +52,9 @@ const Project = styled.div`
     }
 
     svg {
-      margin-right: 5px;
+       margin-right: 5px;
+      color: ${({ theme }) => theme.colors.text}; /* or any other visible color */
+      font-size: 1.5em; /* Adjust the size as needed */
     }
   }
 `;
@@ -64,22 +66,17 @@ function Projects() {
 
       {/* Project 1 */}
       <Project>
-        <h3>Lost and Found Web Application</h3>
-        <p>
-          <em>May 2024 - Aug 2024</em>
-        </p>
+        <h3>Parking Management System</h3>
         <ul>
-          <li>Implemented responsive design and user-friendly interface.</li>
-          <li>Ensured secure handling of user data and efficient database management.</li>
-          <li>Successfully deployed on Render for live use.</li>
-          <li>Received positive feedback for functionality and ease of use.</li>
+          <li>Built a parking web app with React and Node.js for optimized performance and seamless functionality.
+        </li>
           <li>
-            <strong>Technologies:</strong> HTML, CSS, JavaScript, Node.js, SQL.
+            <strong>Technologies:</strong> React HTML, CSS, JavaScript, Node.js, MongoDB.
           </li>
         </ul>
         <div className="project-links">
           <a
-            href="https://github.com/amirkhalifa285/Lost-And-Found-Project"
+            href="https://github.com/ayham10/Bingo-Parking"
             target="_blank"
             rel="noopener noreferrer"
             className="project-link"
@@ -91,28 +88,35 @@ function Projects() {
 
       {/* Project 2 */}
       <Project>
-        <h3>DrinkSmart Application Design and SRS</h3>
+        <h3>Recycling Rewards App</h3>
         <p>
           <em>2022</em>
         </p>
         <ul>
-          <li>Completed the project 2 weeks ahead of schedule.</li>
+          <li>Designed and developed a recycling app with a reward system using React, Node.js, and MongoDB.</li>
           <li>
-            Presented the project to the head of the Software Engineering department, receiving commendation for innovation and usability.
+            Integrated IoT-enabled recycling bins for real-time action verification, improving system reliability.
           </li>
+        </ul>
+        <div className="project-links">
+            <FaGithub /> View Repository
+          </a>
+        </div>
+      </Project>
+      { /* project 3 */}
+      <Project>
+        <h3>Cost Manager Tool</h3>
+        <ul>
+          <li>Engineered a cost management system integrating Java with mySQL databases, ensuring effcient data handling.</li>
+          
+         
           <li>
-            Developed a smart bottle application prototype to track water intake and encourage hydration.
-          </li>
-          <li>
-            Collaborated with a team to create a comprehensive Software Requirements Specification (SRS) document.
-          </li>
-          <li>
-            <strong>Technologies:</strong> UI/UX Design, Prototyping Tools.
+            <strong>Technologies:</strong> 
           </li>
         </ul>
         <div className="project-links">
           <a
-            href="https://github.com/amirkhalifa285/drinkSmart"
+            href="https://github.com/amirkhalifa285/FlyEase-Backend"
             target="_blank"
             rel="noopener noreferrer"
             className="project-link"
@@ -121,38 +125,14 @@ function Projects() {
           </a>
         </div>
       </Project>
-      { /* project 3 */}
+
+
+
+              { /* project 4 */}
       <Project>
-        <h3>FlyEase Application</h3>
-        <p>
-          <em>Present</em>
-        </p>
+        <h3>Smart Recycling</h3>
         <ul>
-          <li>Designed and implemented a mock airport navigation system with features for location-based navigation and congestion visualization.</li>
-          <li>
-            Developed a backend API using FastAPI with endpoints for managing locations, paths, and dynamic navigation.
-          </li>
-          <li>
-            Utilized Dijkstra's algorithm to compute the shortest path between airport locations based on real-time user input.
-          </li>
-          <li>
-            Created a fully interactive front-end map using React and Fabric.js, supporting dynamic visualization of paths, congestion levels, and highlighted navigation routes.
-          </li>
-          <li>
-            Designed a responsive UI with CSS for seamless user interaction across various devices.
-          </li>
-          <li>
-            Implemented an admin panel for managing map entities such as locations and paths.
-          </li>
-          <li>
-            Integrated asynchronous database operations with SQLAlchemy to handle real-time data updates efficiently.
-          </li>
-          <li>
-            Deployed a congestion monitoring system to display real-time congestion levels using visual indicators on the map.
-          </li>
-          <li>
-            Established a modular structure for scalability and maintainability, incorporating reusable components and controllers.
-          </li>
+          <li>enabling real-time recycling data processing.</li>
           <li>
             <strong>Technologies:</strong> Python (FastAPI), JavaScript (React, Fabric.js), SQLAlchemy, CSS, AsyncIO
           </li>
@@ -168,6 +148,7 @@ function Projects() {
           </a>
         </div>
       </Project>
+              
     </ProjectsSection>
   );
 }
