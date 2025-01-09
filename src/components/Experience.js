66a7a410d9b1266a3c39a8a7f2058cebbf557ff6
ayham10 <58478903+ayham10@users.noreply.github.com>
2025-01-09 23:39:23 +0200

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -41,65 +41,30 @@ function Experience() {
 
       {/* New Job */}
       <Job>
-        <h3>CoolVision</h3>
+        <h3>Poriya Hospital</h3>
         <p>
-          <strong>Network Operations Center (NOC) Engineer</strong> | <em>Aug 2024 - Present</em>
+          <strong>Software Developer</strong> | <em>Jan 2024 - Present</em>
         </p>
         <ul>
           <li>
-            Monitored Opsview, Dynatrace, and Zabbix services while achieving 99.9% uptime at the Pingdom platform.
+            Developed robust software solutions using Angular, C#, and SQL databases, increasing operational efciency by 30%.
           </li>
           <li>
-            Managed AWS Cloud Resources, performing server resets and maintenance tasks within Amazon Web Services environments.
+            Conducted 50+ QA tests, reducing post-deployment errors by 25%
           </li>
           <li>
-            Conducted Manual QA Testing on multiple company websites to identify and resolve functionality issues.
-          </li>
-          <li>
-            Troubleshot and resolved technical issues promptly to minimize downtime and service disruptions.
-          </li>
-          <li>
-            Collaborated with cross-functional teams to implement system upgrades and optimize performance.
-          </li>
-          <li>
-            Developed and maintained operational documentation for procedures, incident reports, and best practices.
-          </li>
-          <li>
-            Implemented monitoring strategies to proactively detect and address potential system vulnerabilities.
-          </li>
-          <li>
-            Analyzed system logs and metrics to identify trends and prevent future incidents.
-          </li>
-          <li>
-            Supported deployment processes by coordinating with development teams to roll out new features and services.
+            Delivered scalable applications supporting 100+ hospital staff in daily workflows.
           </li>
         </ul>
       </Job>
 
-      {/* Existing Job 1 */}
-      <Job>
-        <h3>Israel Standardization Institute</h3>
-        <p>
-          <strong>Gold Department IT, Tel Aviv</strong> | <em>May 2023 - Aug 2024</em>
-        </p>
-        <ul>
-          <li>
-            Handled technical issues with laboratory and office equipment within the department.
-          </li>
-          <li>
-            Calculated net weight of gold processed, ensuring 100% accuracy through precise measurements and verification processes.
-          </li>
-          <li>
-            Managed orders for over 50 importers using an organized inventory management system, effectively managing their orders.
-          </li>
-        </ul>
-      </Job>
+      
 
       {/* Existing Job 2 */}
       <Job>
         <h3>Femi Premium</h3>
         <p>
-          <strong>SQL-Technician, Ben-Gurion Airport</strong> | <em>Jan 2022 - Jun 2022</em>
+          <strong>SQL-Technician, Ben-Gurion Airport</strong> | <em>May 2021 - Jun 2022</em>
         </p>
         <ul>
           <li>
@@ -113,26 +78,6 @@ function Experience() {
           </li>
           <li>
             Collaborated with the Ministry of Health and five major laboratories, improving collaboration efficiency by 25%.
-          </li>
-        </ul>
-      </Job>
-
-      {/** Existing Job 3 */}
-
-      <Job>
-        <h3>Yes TV</h3>
-        <p>
-          <strong>Technical Support Agent, Babcom Centers</strong> | <em>Jun 2019 - Nov 2020</em>
-        </p>
-        <ul>
-          <li>
-            Provided technical support for company devices, achieving a customer satisfaction rate of 90% by effectively resolving issues.
-          </li>
-          <li>
-            Improved task completion speed by 30% using the Wizard Windows application.
-          </li>
-          <li>
-            Maintained a 4.8/5 customer satisfaction rating through effective problem-solving techniques and clear communication.
           </li>
         </ul>
       </Job>
